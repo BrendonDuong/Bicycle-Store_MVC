50c36e17f91403dab88a057c4c407f1601e42806
BrendonDuong <114231562+BrendonDuong@users.noreply.github.com>
2023-04-27 08:33:01 +0700

    Update orderdetails.php

--- orderdetails.php
+++ orderdetails.php
@@ -15,14 +15,14 @@
   }
   $ct = new cart();
   if(isset($_GET['confirmid'])){
-	echo "<script>window.location = 'https://bicycle-store-app.herokuapp.com/orderdetails.php'</script>";
+	echo "<script>window.location = 'orderdetails.php'</script>";
 	$orderId = $_GET['confirmid'];
 	$time = $_GET['time'];
 	$price = $_GET['price'];
 	$shifted_confirm = $ct->shifted_confirm($orderId,$time,$price);
 }
 if(isset($_GET['cancelorderid'])){
-	echo "<script>window.location = 'https://bicycle-store-app.herokuapp.com/orderdetails.php'</script>";
+	echo "<script>window.location = 'orderdetails.php'</script>";
 	$orderId = $_GET['cancelorderid'];
 	$time = $_GET['time'];
 	$price = $_GET['price'];
