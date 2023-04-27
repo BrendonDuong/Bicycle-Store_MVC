0a1224f379bb77b6aba4ea89e636bd2bf3ed7859
BrendonDuong <114231562+BrendonDuong@users.noreply.github.com>
2023-04-27 08:15:34 +0700

    Update success.php

--- success.php
+++ success.php
@@ -7,7 +7,7 @@
         $customerId = Session::get('customer_customerId');
 		$insertOrder = $ct->insertOrder($customerId);
 		$delCart = $ct->del_all_data_cart();
-		echo "<script>window.location.href ='https://bicycle-store-app.herokuapp.com/success.php';</script>";
+		echo "<script>window.location.href ='success.php';</script>";
 		//header('Location:success.php');
 	}
 	// if(isset($_GET['partnerCode'])){
