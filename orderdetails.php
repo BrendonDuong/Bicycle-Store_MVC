c3aab7a5a03a5ac277bb98c31d973995f57afc5e
BrendonDuong <114231562+BrendonDuong@users.noreply.github.com>
2023-05-04 10:14:18 +0700

    Update orderdetails.php

--- orderdetails.php
+++ orderdetails.php
@@ -7,7 +7,27 @@
     width: 500px;
 }
 </style>  
-
+<?php 
+    $login_check = Session::get('customer_login');
+    if($login_check==false){
+		//echo "<script>window.location.href ='login.php';</script>";//header('Location:login.php');
+  }
+  $ct = new cart();
+  if(isset($_GET['confirmid'])){
+	echo "<script>window.location = 'orderdetails.php'</script>";
+	$orderId = $_GET['confirmid'];
+	$time = $_GET['time'];
+	$price = $_GET['price'];
+	$shifted_confirm = $ct->shifted_confirm($orderId,$time,$price);
+}
+if(isset($_GET['cancelorderid'])){
+	//echo "<script>window.location = 'orderdetails.php'</script>";
+	$orderId = $_GET['cancelorderid'];
+	$time = $_GET['time'];
+	$price = $_GET['price'];
+	$cancel_order = $ct->cancel_order($orderId,$time,$price);
+}
+?>
  <div class="main">
     <div class="content">
     	<div class="cartoption">		
