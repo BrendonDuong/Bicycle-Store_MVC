d1e6ed54fe55cdf18308445a0b7f1b9287a9623e
BrendonDuong <114231562+BrendonDuong@users.noreply.github.com>
2023-05-04 10:20:32 +0700

    Update orderdetails.php

--- orderdetails.php
+++ orderdetails.php
@@ -8,10 +8,7 @@
 }
 </style>  
 <?php 
-    $login_check = Session::get('customer_login');
-    if($login_check==false){
-		echo "<script>window.location ='login.php';</script>";//header('Location:login.php');
-  }
+    
   $ct = new cart();
   if(isset($_GET['confirmid'])){
 	echo "<script>window.location = 'orderdetails.php'</script>";
