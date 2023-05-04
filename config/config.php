4c4fee945502fedfd63afc325af0166ac11b9fbb
BrendonDuong <114231562+BrendonDuong@users.noreply.github.com>
2023-05-04 10:55:02 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 define("DB_HOST", "us-cdbr-east-06.cleardb.net");
-define("DB_USER", "b08505a4d54f1b");
-define("DB_PASS", "c324ebea");
-define("DB_NAME", "heroku_e12e31de7b90f8d");
+define("DB_USER", "b8d695bc8c7a82");
+define("DB_PASS", "51cb011e");
+define("DB_NAME", "heroku_7000cdc2ba54eb2");
 ?>
