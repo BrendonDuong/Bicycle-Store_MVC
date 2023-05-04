e1ae3b7444fd39e5dc0a6808b4955a7ba3b57582
BrendonDuong <114231562+BrendonDuong@users.noreply.github.com>
2023-05-04 10:26:45 +0700

    Update orderdetails.php

--- orderdetails.php
+++ orderdetails.php
@@ -8,22 +8,9 @@
 }
 </style>  
 <?php 
-    
-  $ct = new cart();
-  if(isset($_GET['confirmid'])){
-	echo "<script>window.location = 'orderdetails.php'</script>";
-	$orderId = $_GET['confirmid'];
-	$time = $_GET['time'];
-	$price = $_GET['price'];
-	$shifted_confirm = $ct->shifted_confirm($orderId,$time,$price);
-}
-if(isset($_GET['cancelorderid'])){
-	echo "<script>window.location = 'orderdetails.php'</script>";
-	$orderId = $_GET['cancelorderid'];
-	$time = $_GET['time'];
-	$price = $_GET['price'];
-	$cancel_order = $ct->cancel_order($orderId,$time,$price);
-}
+        if($login_check==false){
+		echo "<script>window.location ='login.php';</script>";//header('Location:login.php');
+  }
 ?>
  <div class="main">
     <div class="content">
